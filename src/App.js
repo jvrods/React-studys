d19f8900222eb28575b40728f37bbcb81276c36a
João Victor Rodrigues <joaorodriguesvictor@outlook.com>
2021-09-13 17:51:48 -0300

    começo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+           <div className="container">
+               <div className="valid-keys">
+                   <span className="matched"></span>
+                   <span className="remainder"></span>
+               </div>
+            <div className="typer-keys"></div>
+            <div className="completed-words">
+                <ol>
+                    <li>cidade</li>
+                    <li>carro</li>
+                </ol>
+            </div>
+           </div>
+        );
+};
 
 export default App;
